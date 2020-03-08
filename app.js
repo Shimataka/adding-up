56a0716c10fdcfdf9bf048e856f3a63e16b3e5ac
Shimataka <kaleidoscope0817@gmail.com>
2020-03-08 23:35:54 +0900

    Complete to implement inverse ranking

--- app.js
+++ app.js
@@ -35,10 +35,10 @@ rl.on('close', () => {
     }
     const rankingArray = Array.from(prefectureDataMap).sort(
         // 比較関数(=>の後ろが正負0でpair1,2の入れ替え方を変える)
-        (pair1, pair2) => pair2[1].change - pair1[1].change
+        (pair1, pair2) => pair1[1].change - pair2[1].change
     );
-    const rankingstrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popul0 + '=>' + value.popul5 + ' increase rate:' + value.change;
+    const rankingstrings = rankingArray.map(([key, value], i) => {
+        return (i+1) + '-th' + key + ': ' + value.popul0 + '=>' + value.popul5 + ' increase rate:' + value.change;
     })
     console.log(rankingstrings);
 });
